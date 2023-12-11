6ef5a89d34e5448ba5432e52fe8ca1787d56d2a3
VedantMahadik23 <147643855+VedantMahadik23@users.noreply.github.com>
2023-12-11 15:15:25 +0530

    Add files via upload

---
+++ Calculation.java
@@ -0,0 +1,10 @@
+public class Calculation{
+   public static void main(String[] args){
+      int i = 300;
+      long l = i;       //no explicit type casting
+      float f = l;     //no explicit type casting
+      System.out.println("int value :"+i);
+      System.out.println("long value :"+l);
+      System.out.println("float value :"+f);
+   }
+}
